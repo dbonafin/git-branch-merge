6b5669fb132dda840d71adfc49c8b3586bc8221b
E-Lop <edmondolopez@gmail.com>
2022-08-22 17:06:57 +0200

    fixed server.php local

--- server.php
+++ server.php
@@ -3,57 +3,57 @@
         [
             'name' => 'Pisa',
             'country' => 'Italy',
-            'picture' => `https://upload.wikimedia.org/wikipedia/commons/thumb/f/f4/Pisa_-_veduta_dall%27aereo_4.JPG/1024px-Pisa_-_veduta_dall%27aereo_4.jpg`
+            'picture' => `https://static2-viaggi.corriereobjects.it/wp-content/uploads/2015/06/pisa-getty-1080x794.jpg?v=136980`
         ],
         [
             'name' => 'Roma',
             'country' => 'Italy',
-            'picture' => `https://it.wikipedia.org/wiki/Roma#/media/File:Roma_dall'aereo.JPG`
+            'picture' => `https://www.italia.it/content/dam/tdh/it/interests/lazio/roma/roma-in-48-ore/media/20220127150143-colosseo-roma-lazio-shutterstock-756032350.jpg.transform/w_1600/h_900/image.jpeg`
         ],
         [
             'name' => 'Napoli',
             'country' => 'Italy',
-            'picture' => `https://it.wikipedia.org/wiki/Citt%C3%A0_metropolitana_di_Napoli#/media/File:Isla_de_Isquia_Bah%C3%ADa_de_N%C3%A1poles_y_Vesubio.jpg`
+            'picture' => `https://siviaggia.it/wp-content/uploads/sites/2/2020/08/napoli-a-piedi.jpg`
         ],
         [
             'name' => 'Milano',
             'country' => 'Italy',
-            'picture' => 'https://it.wikipedia.org/wiki/Milano#/media/File:Milano,_Duomo_with_Milan_Cathedral_and_Galleria_Vittorio_Emanuele_II,_2016.jpg'
+            'picture' => `https://img.ilgcdn.com/sites/default/files/styles/xl/public/foto/2022/05/02/1651509563-milano-duomo-1.jpg?_=1651509563`
         ],
         [
             'name' => 'Torino',
             'country' => 'Italy',
-            'picture' => 'https://upload.wikimedia.org/wikipedia/commons/6/62/Turin_monte_cappuccini.jpg'
+            'picture' => `https://images.musement.com/cover/0002/45/view-of-turin-center-with-mole-antonelliana-italy_header-144980.jpeg`
         ],
         [
             'name' => 'Ascoli',
             'country' => 'Italy',
-            'picture' => 'https://it.wikipedia.org/wiki/Ascoli_Piceno#/media/File:Ascoli_Piceno_2015_by-RaBoe_069.jpg'
+            'picture' => `https://www.italia.it/content/dam/tdh/it/interests/marche/ascoli-piceno/ascoli-piceno/media/20210309144349-ascoli-piceno.jpg.transform/w_1600/h_480/image.jpeg`
         ],
         [
             'name' => 'New York',
             'country' => 'United States',
-            'picture' => 'https://upload.wikimedia.org/wikipedia/commons/f/f7/Lower_Manhattan_skyline_-_June_2017.jpg'
+            'picture' => `https://www.viaggi-usa.it/wp-content/uploads/2013/11/cosa-fare-a-new-york.jpg`
         ],
         [
             'name' => 'Los Angeles',
             'country' => 'United States',
-            'picture' => 'https://it.wikipedia.org/wiki/Los_Angeles#/media/File:Los_Angeles_with_Mount_Baldy.jpg'
+            'picture' => `https://www.tusoperator.it/wp-content/uploads/2015/06/LOS-ANGELES_cosa-vedere-assolutamente-in-un-giorno.jpg`
         ],
         [
             'name' => 'Las Vegas',
             'country' => 'United States',
-            'picture' => 'https://it.wikipedia.org/wiki/Las_Vegas#/media/File:Las_Vegas_strip.jpg'
+            'picture' => `https://www.viaggi-usa.it/wp-content/uploads/2014/03/Cosa-vedere-a-Las-Vegas.jpg`
         ],
         [
             'name' => 'Washington DC',
             'country' => 'United States',
-            'picture' => 'https://it.wikipedia.org/wiki/Washington#/media/File:DCmontage3.jpg'
+            'picture' => `https://www.flytap.com/-/media/Flytap/new-tap-pages/destinations/north-america/united-states/washington/destinations-washington-banner-mobile-1024x553.jpg`
         ],
         [
             'name' => 'San Francisco',
             'country' => 'United States',
-            'picture' => 'https://it.wikipedia.org/wiki/San_Francisco#/media/File:GoldenGateBridge-001.jpg'
+            'picture' => `https://static2-viaggi.corriereobjects.it/wp-content/uploads/2015/06/san-francisco1.jpg?v=134092`
         ]
     ];
     header('Content-Type: application/json');
